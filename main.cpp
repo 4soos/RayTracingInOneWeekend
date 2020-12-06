5b8103d1d4993f412d926b5f3672d53891eec145
Young <young_2017@126.com>
2020-12-07 00:00:20 +0800

    Creating First Raytraced Image

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 #include "src/color.h"
 #include "src/ray.h"
 
+bool hit_sphere(const point3 &center, double radius, const ray &ray) {
+    vec3 oc = ray.orign() - center;
+    auto a = dot(ray.direction(), ray.direction());
+    auto b = 2.0 * dot(oc, ray.direction());
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray &ray) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, ray))
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(ray.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
